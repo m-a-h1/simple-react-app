3ace505b5922c15c658787c3c3273c5e42d0b18c
amir <amirtnt80@gmail.com>
2021-12-09 17:26:35 +0330

    initial commit

---
+++ src/router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+
+import SigninScreen from "./screen/signin.js";
+// import DashboardScreen from "./screen/dashboard.js";
+
+function AppRouter() {
+  return <div>hello router</div>;
+}
+
+export default AppRouter;
---
+++ src/screen/signin.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+// import { useNavigate } from "react-router-dom";
+
+// import { Form, Input, Button, Row, Col, message } from "antd";
+// import axios from "axios";
+
+function SigninScreen() {
+  return <div>hello sign in</div>;
+}
+
+export default SigninScreen;
